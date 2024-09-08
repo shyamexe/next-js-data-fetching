5ac62d97b6e8432ef3942db32bc867d7bdc3f388
shyam <shyamjith38@hotmail.com>
2024-09-08 16:23:20 +0530

    Fetching Data with Server Components

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Next js Data Fetching
+
+- Fetching Data with Server Components
+
+-------------------------
+
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
---
+++ src/app/users/page.tsx
@@ -0,0 +1,34 @@
+
+type User ={
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    phone: string,
+};
+export default async function UsersPage(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    console.log(users);
+    return (
+        <div className="grid grid-cols-2 gap-2 p-4">
+            {users.map((user:User)=>(
+                <div key={user.id}
+                    className="flex items-center justify-between p-4 bg-white shadow rounded-lg text-slate-800"
+                >
+
+                    <div className="flex flex-col space-y-1">
+                        <h2 className="text-lg font-semibold items-end">{user.name}</h2> 
+                        <p className="text-sm">{user.username}</p>
+                    </div>
+                    <div className="flex flex-col space-y-1 items-end">
+                        <div className="text-sm">{user.email}</div>
+                        <div className="text-sm">{user.phone}</div>
+                    </div>
+
+                    
+                </div>
+            ))}
+        </div>
+    );
+}
